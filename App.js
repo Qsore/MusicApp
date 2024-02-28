04caeb0d24a74bcc5516d984f7e8780a89f88873
Qsore <65732081+Qsore@users.noreply.github.com>
2024-02-28 00:34:46 -0500

    first commit

--- App.js
+++ App.js
@@ -2,19 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name = '01 Metro Boomin = I Am...';
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.innerContainer}/>
+        <Text style={styles.title}>
+          <Text style={styles.subtitle}>Reproduciendo: </Text> {name}
+        </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10,
+    marginTop:100,
+    backgroundColor: '#6424CC',
+    borderRadius:5,
   },
+  innerContainer:{
+    backgroundColor:"#9954ff",
+    height:50,
+    width:150,
+    borderTopLeftRadius:5,
+    borderBottomLeftRadius:5,
+  },
+  title:{
+    fontSize:18,
+    fontWeight:'200',
+    color:'#fff',
+    position: "absolute",
+    backgroundColor: "transparent",
+    top:12,
+    left:10,
+  },
+  subtitle:{
+    fontWeight:"bold",
+  }
 });
